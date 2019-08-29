df656f0fdd2815b867ed07a2ea4a6500acd69b12
dianecdr <diane.cordier44@gmail.com>
2019-08-29 15:07:05 +0200

    Add select department draft funciton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+
+//Import font awesome icon (see how to create a library ..)
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 import data from './data/liste-et-localisation-des-musees-de-france.json';
+
 
 
 function App() {
@@ -17,13 +20,15 @@ function App() {
 class MuseumRow extends React.Component{
   render(){
     const museum=this.props.museum;
-    const museumURL="http://"+museum.fields.sitweb;
-    console.log(museum.fields.sitweb);
+    const museumURL="http://"+museum.fields.sitweb; 
     return(   
       <tr>  
         <td>{this.props.num}</td>
         <td>{museum.fields.nom_du_musee}</td>
         <td>{museum.fields.adr} {museum.fields.cp} {museum.fields.ville}</td>
+        <td>{museum.fields.nomdep}</td>
+        <td>{museum.fields.telephone1}</td>
+        <td>{museum.fields.periode_ouverture}</td>
         <td>
           <a href={museumURL}><button >Site Web</button></a>
         </td>
@@ -33,14 +38,26 @@ class MuseumRow extends React.Component{
 }
 
 class MuseumTable extends React.Component{  
+  constructor(props){
+    super(props);
+    this.handleSelectedDptChange=this.handleSelectedDptChange.bind(this);
+  }
+
+  handleSelectedDptChange(e){
+    this.props.onSelectedDptChange(e.target.value);
+  }
+  
   render(){
+    const options = ["AIN", "ALLIER"];
     const museums = this.props.museums;
     const rows=[];
     let cpt=1;
 
     museums.forEach((museum)=>{
-      let nameToSearch = museum.fields.nom_du_musee.toLowerCase();
-      if (nameToSearch.indexOf(this.props.filterText.toLowerCase()) === -1) {
+      let nameInspected = museum.fields.nom_du_musee.toLowerCase();
+      if ((nameInspected.indexOf(this.props.filterText.toLowerCase()) === -1) 
+      ||
+        (museum.fields.nomdep.indexOf(this.props.selectedDpt)=== -1)) {
         return;
       }
       rows.push(
@@ -56,7 +73,16 @@ class MuseumTable extends React.Component{
             <th>ID</th>
             <th>Nom</th>
             <th>Adresse</th>
-            <th>Site Web</th>
+            <th>
+              Département
+              <select value={this.props.selectedDpt} onChange={this.handleSelectedDptChange}>
+                <option value={options[0]}>{options[0]}</option>
+                <option value={options[1]}>{options[1]}</option>
+              </select>
+            </th>
+            <th>Téléphone</th>
+            <th>Horaires d'ouverture</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -83,8 +109,8 @@ class SearchBar extends React.Component{
     return(
       <div>
         <form >
-          <input type="text" placeholder="Rechercher.." value={filterText} onChange={this.handleFilterTextChange}/>
-          <button type="submit"><FontAwesomeIcon icon={faSearch}/></button>
+          <FontAwesomeIcon icon ={faSearch} />
+          <input type="text" placeholder="Rechercher par nom.." value={filterText} onChange={this.handleFilterTextChange}/>
         </form>
       </div>
     );
@@ -95,9 +121,11 @@ class FilterableMuseumTable extends React.Component{
   constructor(props){
     super(props)
     this.state={
-      filterText:""
+      filterText:"",
+      selectedDpt:"ALLIER"
     };
     this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleSelectedDptChange = this.handleSelectedDptChange.bind(this);
   } 
 
   handleFilterTextChange(filterText) {
@@ -106,16 +134,22 @@ class FilterableMuseumTable extends React.Component{
     });
   }
 
+  handleSelectedDptChange(selectedDpt){
+    this.setState({
+      selectedDpt: selectedDpt
+    });
+  }
+
   render(){
-    /* const MUSEUMS = [
-      {id: 1, name: "Musée des manufactures de Dentelles", address: "14 avenue de la gare", website:"http://www.ville-retournac.fr/musee"},
-      {id: 2, name: "Musée des Beaux Arts et d'Archéologie", address: "Place du palais, CHAUMONT", website:"http://www.ville-chaumont.fr"}
-    ]; */
     return(
       <div>
         <h1>Liste des musées de France</h1>
         <SearchBar filterText={this.state.filterText} onFilterTextChange={this.handleFilterTextChange}/>
-        <MuseumTable museums={this.props.source} filterText={this.state.filterText}/>
+        <MuseumTable 
+          museums={this.props.source} 
+          filterText={this.state.filterText} 
+          selectedDpt={this.state.selectedDpt}
+          onSelectedDptChange={this.handleSelectedDptChange} />
       </div>
     );
   }
